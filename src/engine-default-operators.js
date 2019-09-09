3fa360fd4395485e43f1fa1d498a562a1e8d2b3b
Andrea Leo <andrealeo83@users.noreply.github.com>
2019-09-09 12:23:52 +0200

    fix in notIn operator

--- src/engine-default-operators.js
+++ src/engine-default-operators.js
@@ -5,8 +5,8 @@ import Operator from './operator'
 let Operators = []
 Operators.push(new Operator('equal', (a, b) => a === b))
 Operators.push(new Operator('notEqual', (a, b) => a !== b))
-Operators.push(new Operator('in', (a, b) => b.indexOf(a) > -1))
-Operators.push(new Operator('notIn', (a, b) => b.indexOf(a) === -1))
+Operators.push(new Operator('in', (a, b) => a.indexOf(b) > -1))
+Operators.push(new Operator('notIn', (a, b) => a.indexOf(b) === -1))
 
 Operators.push(new Operator('contains', (a, b) => a.indexOf(b) > -1, Array.isArray))
 Operators.push(new Operator('doesNotContain', (a, b) => a.indexOf(b) === -1, Array.isArray))
